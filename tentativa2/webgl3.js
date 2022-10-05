a96cfc8e88304502d90322b245750f3f3d670385
Pmwalter2 <patrick.menegassi@hotmail.com>
2022-10-05 17:17:17 -0300

    Uga

--- tentativa2/webgl3.js
+++ tentativa2/webgl3.js
@@ -191,62 +191,7 @@ function main() {
   // Tell the twgl to match position with a_position, n
   // normal with a_normal etc..
   twgl.setAttributePrefix("a_");
-  var indices = new Uint16Array([
-    0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14,
-    15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
-  ]);
   //cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 1);
-  var arrays_pyramid = {
-    position: new Float32Array([
-      0, 1, 0,
-
-      -1, -1, 1,
-
-      1, -1, 1,
-
-      0, 1, 0,
-
-      1, -1, 1,
-
-      1, -1, -1,
-
-      0, 1, 0,
-
-      1, -1, -1,
-
-      -1, -1, -1,
-
-      0, 1, 0,
-
-      -1, -1, -1,
-
-      -1, -1, 1,
-    ]),
-
-    // texcoord: new Float32Array([
-    //   1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1,
-    //   1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1,
-    //   0.5, 0.5, 1, 1, 0.5, 0.5, 1, 1, 0.5, 0.5, 1, 1, 0.5, 0.5, 1, 1, 0, 1, 1,
-    //   1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0,
-    //   0, 1, 1,
-    // ]),
-    indices: new Uint16Array([
-      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 8, 4, 1, 8, 5, 2,
-    ]),
-    normal: new Float32Array([
-      1, -1, 1,
-
-      1, -1, 1,
-
-      1, 1, 1,
-
-      -1, 1, 1,
-
-      -1, -1, -1,
-
-      -1, 1, -1,
-    ]),
-  };
 
   var arrays_cube = {
     // vertex positions for a cube
@@ -267,7 +212,7 @@ function main() {
       0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12,
       14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
     ]),
-  };
+  }; 
   cubeBufferInfo = twgl.createBufferInfoFromArrays(gl, arrays_cube);
 
   // setup GLSL program
@@ -300,7 +245,7 @@ function main() {
   function drawScene(time) {
     time *= 0.001;
     config.time = time;
-    console.log(time);
+    
 
     twgl.resizeCanvasToDisplaySize(gl.canvas);
 
