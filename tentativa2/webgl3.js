692f651ae2e0d5b7c43c43dcd663ff34c59deffc
Pmwalter2 <patrick.menegassi@hotmail.com>
2022-10-04 21:22:05 -0300

    Ta complicado

--- tentativa2/webgl3.js
+++ tentativa2/webgl3.js
@@ -1,18 +1,13 @@
 "use strict";
 
 var vs = `#version 300 es
-
 in vec4 a_position;
 in vec4 a_color;
-
 uniform mat4 u_matrix;
-
 out vec4 v_color;
-
 void main() {
   // Multiply the position by the matrix.
   gl_Position = u_matrix * a_position;
-
   // Pass the color to the fragment shader.
   v_color = a_color;
 }
@@ -20,22 +15,15 @@ void main() {
 
 var fs = `#version 300 es
 precision highp float;
-
 // Passed in from the vertex shader.
 in vec4 v_color;
-
 uniform vec4 u_colorMult;
 uniform vec4 u_colorOffset;
-
 out vec4 outColor;
-
 void main() {
    outColor = v_color * u_colorMult + u_colorOffset;
 }
 `;
-
-
-// INTERFACE ---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE------------------------------------
 
 const degToRad = (d) => (d * Math.PI) / 180;
 
@@ -45,66 +33,38 @@ var config = {
   rotate: degToRad(20),
   x: 0,
   y: 0,
-  addFrenteCube: function () {
-    countF++;
-
-    mioca.children.push({
-      name: `mioca${countF}`,
-      translation: [countF - 1, 0, 0],
-    });
-    objectsToDraw = [];
-    objects = [];
-    nodeInfosByName = {};
-    scene = makeNodeCube(mioca);
-
-    
-    return;
-  },
-  addCimaCube: function () {
+  rotation: 0,
+  camera_x: 4,
+  addCaixa: function () {
     countC++;
 
-    mioca.children.push({
-      name: `miocaCima${countC}`,
+    objeto.children.push({
+      name: `cubo${countC}`,
       translation: [0, countC, 0],
     });
+
     objectsToDraw = [];
     objects = [];
     nodeInfosByName = {};
-    scene = makeNodeCube(mioca);
-
-    
-    return;
+    scene = makeNode(objeto);
   },
-  addFrenteCone: function(){
-    countF++;
-
-    mioca.children.push({
-      name: `mioca${countF}`,
-      translation: [countF - 1, 0, 0],
-    });
-    objectsToDraw = [];
-    objects = [];
-    nodeInfosByName = {};
-    scene = makeNodeCone(mioca);
-
-    
-    return;
-  }
+  triangulo: 0,
+  criarVertice: function () {},
+  time: 0.0,
 };
 
-const loadGUI = (gl) => {
+const loadGUI = () => {
   const gui = new dat.GUI();
-  gui.add(config, "rotate", 0, 1000, 1); //Minimo, maximo e "velocidade"
-  gui.add(config, "x", 1, 10, 1);
-  gui.add(config, "y", 1, 10, 0.1);
-  gui.add(config, "addFrenteCube");
-  gui.add(config, "addCimaCube");
-  gui.add(config, "addFrenteCone");
-
-  //gui.add(config, "teste", 0, 100);
+  gui.add(config, "rotate", 0, 20, 0.5);
+  gui.add(config, "x", -150, 150, 5);
+  gui.add(config, "y", -100, 100, 5);
+  gui.add(config, "rotation", -1000, 1000, 10);
+  gui.add(config, "addCaixa");
+  gui.add(config, "camera_x", 0, 20, 0.5);
+  gui.add(config, "triangulo", 0, 20, 0.5);
+  gui.add(config, "criarVertice");
+  gui.add(config, "time", 0, 100);
 };
-
-// INTERFACE ---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE---INTERFACE------------------------------------
 
 var TRS = function () {
   this.translation = [0, 0, 0];
@@ -171,24 +131,23 @@ Node.prototype.updateWorldMatrix = function (matrix) {
   });
 };
 
-// VARIAVEIS PARA USO NO MAIN ----------------------------------------------------------------------------------
-
+var cubeVAO;
+var cubeBufferInfo;
 var objectsToDraw = [];
 var objects = [];
 var nodeInfosByName = {};
 var scene;
-var mioca = {};
+var objeto = {};
 var countF = 0;
 var countC = 0;
 var programInfo;
-var cubeBufferInfo;
-var cubeVAO;
-var coneBufferInfo;
-var coneVAO;
 
-// VARIAVEIS PARA USO NO MAIN ----------------------------------------------------------------------------------
+//CAMERA VARIABLES
+var cameraPosition;
+var target;
+var up;
 
-function makeNodeCube(nodeDescription) {
+function makeNode(nodeDescription) {
   var trs = new TRS();
   var node = new Node(trs);
   nodeInfosByName[nodeDescription.name] = {
@@ -196,17 +155,14 @@ function makeNodeCube(nodeDescription) {
     node: node,
   };
   trs.translation = nodeDescription.translation || trs.translation;
-  //trs.scale = trs.scale;
   if (nodeDescription.draw !== false) {
     node.drawInfo = {
       uniforms: {
-        u_colorOffset: [0, 0, 0.5, 0],
-        u_colorMult: [0.4, 0.4, 0.4, 1],
+        u_colorOffset: [0.2, 0.2, 0.7, 0],
+        u_colorMult: [0.4, 0.1, 0.4, 1],
       },
       programInfo: programInfo,
-      // Alterar buffer info para a forma desejada
       bufferInfo: cubeBufferInfo,
-      // Alterar vertexArray para a informação desejada
       vertexArray: cubeVAO,
     };
     objectsToDraw.push(node.drawInfo);
@@ -218,44 +174,9 @@ function makeNodeCube(nodeDescription) {
   return node;
 }
 
-function makeNodeCone(nodeDescription) {
-  var trs = new TRS();
-  var node = new Node(trs);
-  nodeInfosByName[nodeDescription.name] = {
-    trs: trs,
-    node: node,
-  };
-  trs.translation = nodeDescription.translation || trs.translation;
-  //trs.scale = trs.scale;
-  if (nodeDescription.draw !== false) {
-    node.drawInfo = {
-      uniforms: {
-        u_colorOffset: [0.5, 0.5, 1, 1],
-        u_colorMult: [0, 0.4, 0.4, 1],
-      },
-      programInfo: programInfo,
-      // Alterar buffer info para a forma desejada
-      bufferInfo: coneBufferInfo,
-      // Alterar vertexArray para a informação desejada
-      vertexArray: coneVAO,
-    };
-    objectsToDraw.push(node.drawInfo);
-    objects.push(node);
-  }
-  makeNodesCone(nodeDescription.children).forEach(function (child) {
-    child.setParent(node);
-  });
-  return node;
-}
-
 function makeNodes(nodeDescriptions) {
-  return nodeDescriptions ? nodeDescriptions.map(makeNodeCube) : [];
+  return nodeDescriptions ? nodeDescriptions.map(makeNode) : [];
 }
-
-function makeNodesCone(nodeDescriptions) {
-  return nodeDescriptions ? nodeDescriptions.map(makeNodeCube) : [];
-}
-
 function main() {
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
@@ -270,18 +191,89 @@ function main() {
   // Tell the twgl to match position with a_position, n
   // normal with a_normal etc..
   twgl.setAttributePrefix("a_");
+  var indices = new Uint16Array([
+    0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14,
+    15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
+  ]);
+  //cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 1);
+  var arrays_pyramid = {
+    position: new Float32Array([
+      0, 1, 0,
 
-  cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 1);
-  coneBufferInfo   = flattenedPrimitives.createTruncatedConeBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
+      -1, -1, 1,
 
+      1, -1, 1,
+
+      0, 1, 0,
+
+      1, -1, 1,
+
+      1, -1, -1,
+
+      0, 1, 0,
+
+      1, -1, -1,
+
+      -1, -1, -1,
+
+      0, 1, 0,
+
+      -1, -1, -1,
+
+      -1, -1, 1,
+    ]),
+
+    // texcoord: new Float32Array([
+    //   1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1,
+    //   1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1,
+    //   0.5, 0.5, 1, 1, 0.5, 0.5, 1, 1, 0.5, 0.5, 1, 1, 0.5, 0.5, 1, 1, 0, 1, 1,
+    //   1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0,
+    //   0, 1, 1,
+    // ]),
+    indices: new Uint16Array([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 8, 4, 1, 8, 5, 2,
+    ]),
+    normal: new Float32Array([
+      1, -1, 1,
+
+      1, -1, 1,
+
+      1, 1, 1,
+
+      -1, 1, 1,
+
+      -1, -1, -1,
+
+      -1, 1, -1,
+    ]),
+  };
+
+  var arrays_cube = {
+    // vertex positions for a cube
+    position: new Float32Array([
+      1, 1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, -1, -1, -1, -1,
+      -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, 1, -1, -1, 1, -1, -1, -1, -1, 1, -1, -1,
+      1, -1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, 1, -1, 1,
+      1, -1, 1, -1, -1, -1, -1, -1,
+    ]),
+    // vertex normals for a cube
+    normal: new Float32Array([
+      1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,
+      0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,
+      -1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, -1,
+      0, 0, -1,
+    ]),
+    indices: new Uint16Array([
+      0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12,
+      14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
+    ]),
+  };
+  cubeBufferInfo = twgl.createBufferInfoFromArrays(gl, arrays_cube);
 
   // setup GLSL program
-
   programInfo = twgl.createProgramInfo(gl, [vs, fs]);
-  
 
   cubeVAO = twgl.createVAOFromBufferInfo(gl, programInfo, cubeBufferInfo);
-  coneVAO = twgl.createVAOFromBufferInfo(gl, programInfo, coneBufferInfo);
 
   function degToRad(d) {
     return (d * Math.PI) / 180;
@@ -289,39 +281,27 @@ function main() {
 
   var fieldOfViewRadians = degToRad(60);
 
+  objectsToDraw = [];
+  objects = [];
+  nodeInfosByName = {};
+
   // Let's make all the nodes
-
-  mioca = {
-    name: "mioca1",
+  objeto = {
+    name: "cubo0",
     translation: [0, 0, 0],
-    children: [
-      {
-        name: "mioca2",
-        translation: [1, 0, 0],
-      },
-    ],
+    children: [],
   };
-  countF = 2;
 
-  
-
-  //   var scene = makeNode(blockGuyNodeDescriptions);
-  //   nodeInfosByName["left-finger"].trs.scale = [0.5, 2, 0.5];
-  //   nodeInfosByName["handle"].trs.scale = [0.2, 1.5, 0.3];
-  //   nodeInfosByName["handle"].trs.translation = [-1, -0.8, 0];
-  //   nodeInfosByName["hilt"].trs.scale = [2, 0.2, 0.5];
-  //   nodeInfosByName["hilt"].trs.translation = [-1, 0, 0];
-  //   nodeInfosByName["blade"].trs.scale = [0.5, 3.5, 0.1];
-  //   nodeInfosByName["blade"].trs.translation = [-1, 1.8, 0];
-  scene = makeNodeCube(mioca);
+  scene = makeNode(objeto);
 
   requestAnimationFrame(drawScene);
-  
-  //console.log(objects);
+
   // Draw the scene.
   function drawScene(time) {
     time *= 0.001;
-    
+    config.time = time;
+    console.log(time);
+
     twgl.resizeCanvasToDisplaySize(gl.canvas);
 
     // Tell WebGL how to convert from clip space to pixels
@@ -335,9 +315,9 @@ function main() {
     var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 200);
 
     // Compute the camera's matrix using look at.
-    var cameraPosition = [4, 3.5, 10];
-    var target = [0, 4, 0];
-    var up = [0, 1, 0];
+    cameraPosition = [config.camera_x, 3.5, 10];
+    target = [0, 3.5, 0];
+    up = [0, 1, 0];
     var cameraMatrix = m4.lookAt(cameraPosition, target, up);
 
     // Make a view matrix from the camera matrix.
@@ -348,14 +328,9 @@ function main() {
     var adjust;
     var speed = 3;
     var c = time * speed;
-    adjust = degToRad(config.rotate);
-    nodeInfosByName["mioca1"].trs.rotation[0] = adjust;
-    nodeInfosByName["mioca1"].trs.translation[0] = config.x % 6;
-    nodeInfosByName["mioca1"].trs.scale[0] = config.y % 10;
 
-    // adjust = Math.abs(Math.sin(c));
-    // nodeInfosByName["point between feet"].trs.translation[1] = adjust;
-
+    adjust = degToRad(time * config.rotation);
+    nodeInfosByName["cubo0"].trs.rotation[0] = adjust;
     // Update all world matrices in the scene graph
     scene.updateWorldMatrix();
 
@@ -367,16 +342,12 @@ function main() {
       );
     });
 
-  
     // ------ Draw the objects --------
 
     twgl.drawObjectList(gl, objectsToDraw);
 
     requestAnimationFrame(drawScene);
   }
-  
 }
-
-
 
 main();
